5da650781a1d4cac002e477f0bd626889fc7bb48
balint-sizzle <s2064308@ed.ac.uk>
2021-06-09 09:55:37 +0100

    rod angle
    the angle between the rod end and the base now works well.
    
    the builtin function wasn't returning the values i wanted to, not sure why.
    
    makign my own func works now.
    
    neg angle if it falls left and pos if it falls right.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pymunk as pm
 import pygame as pg
 from pygame import gfxdraw
 import pymunk.pygame_util as pmg
+from math import atan2
 
 pg.init()
 canvas = pg.display.set_mode((800,800))
@@ -39,16 +40,18 @@ def move_right(poly):
     poly.body.position = (poly.body.position.x+1, poly.body.position.y)
     pm.Body.update_position(poly.body, 0.1)
 
+def get_angle(b1, b2):
+    return round((atan2(b2.position.y-b1.position.y, b2.position.x-b1.position.x)*180/3.141) + 90, 3)
 space._set_gravity((0,100))
 
-base_w, base_h = 200, 20
+base_w, base_h = 600, 20
 base_b = pm.Body(1, 100, body_type=pm.Body.KINEMATIC)
 circle_b = pm.Body(1, 1, body_type=pm.Body.DYNAMIC)
 
 base = pm.Poly(base_b, rect_shape(base_w, base_h))
 circle = pm.Circle(circle_b, 1)
 
-base_b.position = (400, 400)
+base_b.position = (400, 500)
 circle_b.position = (400, 320)
 
 joint = pm.PinJoint(circle_b, base_b, (0, 0), (0, -base_h/2))
@@ -56,8 +59,9 @@ joint = pm.PinJoint(circle_b, base_b, (0, 0), (0, -base_h/2))
 space.add(circle_b, circle)
 space.add(base_b, base)
 space.add(joint)
+print(circle.body.position)
 
-while True:
+while 1:
     for event in pg.event.get():
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_DOWN:
@@ -74,8 +78,9 @@ while True:
 
     canvas.fill(pg.Color("Grey"))
     
-    space.step(1/500)
+    space.step(1/50)
+    print(get_angle(base.body, circle.body))
 
     space.debug_draw(draw_options)
+
     pg.display.flip()
-    
