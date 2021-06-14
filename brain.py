f8e1b6462aeddadf9a4a1f252db41682070c611a
balint-sizzle <s2064308@ed.ac.uk>
2021-06-14 20:47:20 +0100

    fitness function
    the fitness function looks alright
    there may be issues with the scaling
    
    now i'm going to make a visual representation so it shows the best of the winner generations

--- brain.py
+++ brain.py
@@ -3,8 +3,11 @@
 # output: movement
 # fitness: time alive
 
+from __future__ import print_function
+import os
 import neat
-
+import visualize
+from rod_sim import Jack
 
 config = neat.config.Config(neat.DefaultGenome,
                             neat.DefaultReproduction,
@@ -12,12 +15,17 @@ config = neat.config.Config(neat.DefaultGenome,
                             neat.DefaultStagnation,
                             "config.ini")
 
+sigmoid_scaling_factor = 100
+
 def eval_genome(genomes, config):
     for genome_id, genome in genomes:
         net = neat.nn.FeedForwardNetwork.create(genome, config)
-        time_elapsed = 0
-        while 
-        net.activate(angle)
-        genome.fitness -= (output[0] - xo[0]) ** 2
+        jack = Jack()
+        while jack.time_elapsed < 10 and not jack.fallen:
+            output = net.activate(jack.get_angle())
+            jack.move_base(output * sigmoid_scaling_factor)
+        genome.fitness = jack.time_elapsed
+
+def run)
 population = neat.Population(config)
 
--- rod_sim.py
+++ rod_sim.py
@@ -13,18 +13,18 @@ class Jack:
         space = pm.Space()
 
         space._set_gravity((0,100))
-        base_b = pm.Body(1, 100, body_type=pm.Body.KINEMATIC)
+        self.base_b = pm.Body(1, 100, body_type=pm.Body.KINEMATIC)
         circle_b = pm.Body(1, 1, body_type=pm.Body.DYNAMIC)
 
-        base_b.position = (400, 500)
+        self.base_b.position = (400, 500)
         circle_b.position = (400, 320)
 
-        joint = pm.PinJoint(circle_b, base_b, (0, 0), (0, -self.base_h/2))
-        base = pm.Poly(base_b, self.rect_shape(self.base_w, self.base_h))
+        joint = pm.PinJoint(circle_b, self.base_b, (0, 0), (0, -self.base_h/2))
+        base = pm.Poly(self.base_b, self.rect_shape(self.base_w, self.base_h))
         circle = pm.Circle(circle_b, 1)
 
         space.add(circle_b, circle)
-        space.add(base_b, base)
+        space.add(self.base_b, base)
         space.add(joint)
 
         self.fallen = False
@@ -35,15 +35,16 @@ class Jack:
         return round((atan2(b2.position.y-b1.position.y,
                             b2.position.x-b1.position.x) *180/pi) + 90, 3)
 
-    def step_rod(self):
+    def move_base(self, delta_p):
+        self.move_base(delta_p)
         self.space.step(self.step_size)
         self.time_elapsed += self.step_size
 
         return self.get_angle()
     
+    def move_body(self, amnt):
+        self.base_b.position = (self.base_b.position.x+amnt, self.base_b.position.y)
+
     @staticmethod
     def rect_shape(w, h):
         return [(-w/2, -h/2), (w/2, -h/2), (w/2, h/2), (-w/2, h/2)]
-    @staticmethod
-    def move_body(amnt, poly):
-        poly.body.position = (poly.body.position.x+amnt, poly.body.position.y)
